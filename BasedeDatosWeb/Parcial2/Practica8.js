c889e80e140e83ee86b2b090a0562b678494b4f7
AndreaSalasB <159410815+AndreaSalasB@users.noreply.github.com>
2024-10-08 18:31:31 -0700

    Práctica #8 08/10/24

---
+++ BasedeDatosWeb/Parcial2/Practica8.js
@@ -0,0 +1,13 @@
+$(document).ready( function(){
+    alert("Funciona jquery");
+
+    $('#boton').on("click", function(){
+        var nombre = $('#nombre').val();
+
+        alert(nombre);
+    });
+
+    $('#texto').click(function(){
+        $('#texto').css("display", "inherit").fadeOut(2000);
+    })
+})
